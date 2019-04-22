7c263710b8a3303ee01dab38ad0c12413892464c
Ryan Horn <ryanhorn@cu-rgntgw2-5-10.218.73.102.int.colorado.edu>
2019-04-22 16:20:40 -0600

    added a player struct if we are able to parse player stats from the web

--- NBA.hpp
+++ NBA.hpp
@@ -9,6 +9,15 @@
 #include "curlIN/curl.h"
 using namespace std;
 
+//struct that represents a player
+struct player
+{
+  string position;
+  int age;
+  int height; //total height in inches
+  int weight; //weight in lbs
+};
+
 //struct that represents a team
 struct team
 {
